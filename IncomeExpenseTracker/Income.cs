890467ea4d8f2ebde8a7c403e2f5bc20b4350296
Onkabetse <113599354+Onkabetse077@users.noreply.github.com>
2024-08-21 20:56:41 +0200

    Income

--- IncomeExpenseTracker/Income.cs
+++ IncomeExpenseTracker/Income.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace IncomeExpenseTracker
 {
     public partial class Income : UserControl
     {
+        string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\gosia\Documents\Projects\Visual Studio\C#\IncomeExpenseTracker\tools\Databases\expense.mdf"";Integrated Security=True;Connect Timeout=30";
         public Income()
         {
             InitializeComponent();
+        }
+
+        public void displayCategoryList()
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                connect.Open();
+
+                string selectData = "SELECT * FROM categories WHERE type = @type AND status = @status";
+
+                using (SqlCommand cmd = new SqlCommand(selectData,connect))
+                {
+                    cmd.Parameters.AddWithValue("@type", "Income");
+                    cmd.Parameters.AddWithValue("@status", "Active");
+
+                    cbCategory_Income.Items.Clear();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                }
+            }
+        }
+        private void btnAdd_Income_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
